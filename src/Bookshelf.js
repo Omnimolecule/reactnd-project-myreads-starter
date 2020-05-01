7ef549ebfaee3762882e17e672a8688a540de2fe
Jasmin Frei <jasmin.frei@hotmail.com>
2020-05-01 11:18:35 +0200

    feat: extract bookshelf into a new component

---
+++ src/Bookshelf.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import Book from './Book'
+
+const Bookshelf = (props) => {
+    return (
+        <div className="bookshelf">
+            <h2 className="bookshelf-title">{props.title}</h2>
+            <div className="bookshelf-books">
+                <ol className="books-grid">
+                    {props.books.map((book) => (
+                        <li>
+                            <Book book={book} />
+                        </li>
+                    ))}
+                </ol>
+            </div>
+        </div>
+    )
+}
+
+export default Bookshelf
