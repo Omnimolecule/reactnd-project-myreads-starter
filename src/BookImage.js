4d212bc480e5af02335420030c238351e02f6d8d
Jasmin Frei <jasmin.frei@hotmail.com>
2020-05-01 10:34:20 +0200

    stlye: formatting

--- src/BookImage.js
+++ src/BookImage.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 const BookImage = (props) => {
     return (
-        <div className="book-cover" 
-            style={{ 
-                width: 128, 
-                height: 193, 
+        <div className="book-cover"
+            style={{
+                width: 128,
+                height: 193,
                 backgroundImage: `url("${props.imageUrl}")`
             }} />
     )
