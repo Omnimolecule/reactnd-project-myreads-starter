de4a141906e82a11cc3cc20ed17f5a1a8f951b18
Jasmin Frei <jasmin.frei@hotmail.com>
2020-05-04 10:45:37 +0200

    fix: fixed warning

--- src/BookShelfChanger.js
+++ src/BookShelfChanger.js
@@ -3,12 +3,14 @@ import React from 'react'
 const BookShelfChanger = (props) => {
     return (
         <div className="book-shelf-changer">
-            <select value={props.shelf} onChange={(event) => { props.onChangeShelf(event.target.value) }}>
+            <select
+                value={props.shelf ? props.shelf : 'none'}
+                onChange={(event) => { props.onChangeShelf(event.target.value) }}>
                 <option value="move" disabled>Move to...</option>
                 <option value="currentlyReading">Currently Reading</option>
                 <option value="wantToRead">Want to Read</option>
                 <option value="read">Read</option>
-                <option value="none" selected>None</option>
+                <option value="none">None</option>
             </select>
         </div>
     )
